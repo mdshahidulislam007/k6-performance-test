7272444afb64ca4462904de1fb2a4d850643cf21
Md. Shahidul Islam <105781169+shahidulbs23@users.noreply.github.com>
2025-03-18 20:11:25 +0600

    added helper file

--- endpoints.js
+++ endpoints.js
@@ -3,6 +3,12 @@ export const BASE_URL="";
 export const BASE_EC_URL="";
 export const BEARER_TOKEN = "";
 export const UPDATE_STEP_ACTIVITY="/v1/activity/step/";
+export const STEP_ACTIVITY_DATA="/v1/activity/step/";
+
 export const EVENT_LIST="/v1/event/event-list/";
 export const RUNNING_EVENT_LIST="/v1/event/event-list/";
+export const USER_DASHBOARD="";
+export const NOTIFICATION_COUNT="";
+
+
 
---
+++ helper.js
@@ -0,0 +1,30 @@
+import {BEARER_TOKEN} from "./endpoints";
+
+
+export const loadOptions = {
+    load_scenario: {
+        executor: 'constant-vus',
+        vus: 20,
+        duration: '10s'
+    },
+}
+
+export const headers = {
+    'access-control-allow-origin': '*',
+    'Content-Type': 'application/json',
+    Authorization: BEARER_TOKEN
+}
+
+export let updateActivityPayload = () => {
+    let payload = {
+        activity_list: [
+            {
+                date: "2025-02-23",
+                step_count: 7000,
+                walking_time_minute: "120"
+            }
+        ],
+        recording_type: "manual"
+    }
+    return JSON.stringify(payload);
+}
